b2a51f00263080bdabc3b44091c3e93f49d05057
怡 梁 <1469869780@qq.com>
2025-05-27 00:22:30 +0800

    添加项目文件。

---
+++ BarTsSegmentView.cpp
@@ -0,0 +1,128 @@
+﻿
+// BarTsSegmentView.cpp: CBarTsSegmentView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "BarTsSegment.h"
+#endif
+
+#include "BarTsSegmentDoc.h"
+#include "BarTsSegmentView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CBarTsSegmentView
+
+IMPLEMENT_DYNCREATE(CBarTsSegmentView, CView)
+
+BEGIN_MESSAGE_MAP(CBarTsSegmentView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CBarTsSegmentView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CBarTsSegmentView 构造/析构
+
+CBarTsSegmentView::CBarTsSegmentView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CBarTsSegmentView::~CBarTsSegmentView()
+{
+}
+
+BOOL CBarTsSegmentView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CBarTsSegmentView 绘图
+
+void CBarTsSegmentView::OnDraw(CDC* /*pDC*/)
+{
+	CBarTsSegmentDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CBarTsSegmentView 打印
+
+
+void CBarTsSegmentView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CBarTsSegmentView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CBarTsSegmentView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CBarTsSegmentView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CBarTsSegmentView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CBarTsSegmentView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CBarTsSegmentView 诊断
+
+#ifdef _DEBUG
+void CBarTsSegmentView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CBarTsSegmentView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CBarTsSegmentDoc* CBarTsSegmentView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CBarTsSegmentDoc)));
+	return (CBarTsSegmentDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CBarTsSegmentView 消息处理程序
---
+++ BarTsSegmentView.h
@@ -0,0 +1,52 @@
+﻿
+// BarTsSegmentView.h: CBarTsSegmentView 类的接口
+//
+
+#pragma once
+
+
+class CBarTsSegmentView : public CView
+{
+protected: // 仅从序列化创建
+	CBarTsSegmentView() noexcept;
+	DECLARE_DYNCREATE(CBarTsSegmentView)
+
+// 特性
+public:
+	CBarTsSegmentDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CBarTsSegmentView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // BarTsSegmentView.cpp 中的调试版本
+inline CBarTsSegmentDoc* CBarTsSegmentView::GetDocument() const
+   { return reinterpret_cast<CBarTsSegmentDoc*>(m_pDocument); }
+#endif
+
